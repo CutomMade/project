b84da956c459a235db5c32a3c566b430ebb8faf7
Denzel <denzelhadebe7@gmail.com>
2023-08-29 20:27:27 +0200

    try commit

--- admin_header.php
+++ admin_header.php
@@ -20,7 +20,7 @@ if(isset($message)){
       <nav class="navbar">
          <a href="admin_page.php">home</a>
          <a href="admin_products.php">products</a>
-         <a href="admin_orders.php">orders</a>
+         <a href="admin_orders.php">orderss</a>
          <a href="admin_users.php">users</a>
          <a href="admin_contacts.php">messages</a>
       </nav>
